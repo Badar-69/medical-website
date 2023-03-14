88e918a8a3c437f4b9f71fcfb06089398a1cdb15
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-03-14 21:58:51 +0500

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import lineImg from '../assets/images/line.png'
+import art1Img from '../assets/images/art-1.jpg'
+import art2Img from '../assets/images/art-2.jpg'
+import art3Img from '../assets/images/art-3.jpg'
 
 export default function blog() {
     return (
@@ -8,13 +12,13 @@ export default function blog() {
                     <div className="blog-text">
                         <h2 className='blog-head-blue'>Our Blog</h2>
                         <h3 className="blog-heading">Latest From Blog</h3>
-                        <img className='con-line' src="/images/line.png" alt="" />
+                        <img className='con-line' src={lineImg} alt="" />
                     </div>
                 </div>
 
                 <div className="latest-bg">
                     <div className="article article-1">
-                        <img className='art-img' src="/images/art-1.jpg" alt="" />
+                        <img className='art-img' src={art1Img} alt="" />
                         <div className="art-text">
                             <p className="art-para-1">May 16, 2020</p>
                             <p className="art-para-2">Taylor Life In Other Word</p>
@@ -22,7 +26,7 @@ export default function blog() {
                     </div>
 
                     <div className="article article-2">
-                        <img className='art-img' src="/images/art-2.jpg" alt="" />
+                        <img className='art-img' src={art2Img} alt="" />
                         <div className="art-text">
                             <p className="art-para-1">May 16, 2020</p>
                             <p className="art-para-2">How To Choose The Doctor...</p>
@@ -30,7 +34,7 @@ export default function blog() {
                     </div>
 
                     <div className="article article-3">
-                        <img className='art-img' src="/images/art-3.jpg" alt="" />
+                        <img className='art-img' src={art3Img} alt="" />
                         <div className="art-text">
                             <p className="art-para-1">May 16, 2020</p>
                             <p className="art-para-2">How To Make A Checkup...</p>
